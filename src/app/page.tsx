d8fa1678c5ec8568cd26c60a458f008f1a6ce96f
Dan <dibyjeanyves02@gmail.com>
2026-07-02 16:53:02 +0000

    feat: mise en place de la card (#14)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,94 +1,19 @@
 import { NavbarWrapper } from '@/components/layout/NavbarWrapper';
 import { HeroImage } from '@/components/layout/HeroImage';
 
+
 export default function ShopPage() {
   return (
     <div className="min-h-screen bg-slate-950">
       <NavbarWrapper />
       <HeroImage />
-      <main className="container mx-auto px-4 py-12">
-        <div className='mt-[30]'>
-          <h2>SALUT TEST despace</h2>
-        </div>
-
-        <div className='mt-[30]'>
-          <h2>SALUT TEST despace</h2>
-        </div>
-
-        <div className='mt-[30]'>
-          <h2>SALUT TEST despace</h2>
-        </div>
-
-        <div className='mt-[30]'>
-          <h2>SALUT TEST despace</h2>
-        </div>
-
-        <div className='mt-[30]'>
-          <h2>SALUT TEST despace</h2>
-        </div>
-
-        <div className='mt-[30]'>
-          <h2>SALUT TEST despace</h2>
-        </div>
-
-        <div className='mt-[30]'>
-          <h2>SALUT TEST despace</h2>
-        </div>
-
-        <div className='mt-[30]'>
-          <h2>SALUT TEST despace</h2>
-        </div>
-
-        <div className='mt-[30]'>
-          <h2>SALUT TEST despace</h2>
-        </div>
-
-        <div className='mt-[30]'>
-          <h2>SALUT TEST despace</h2>
-        </div>
-
-        <div className='mt-[30]'>
-          <h2>SALUT TEST despace</h2>
-        </div>
-
-        <div className='mt-[30]'>
-          <h2>SALUT TEST despace</h2>
-        </div>
-
-        <div className='mt-[30]'>
-          <h2>SALUT TEST despace</h2>
-        </div>
-
-        <div className='mt-[30]'>
-          <h2>SALUT TEST despace</h2>
-        </div>
-
-        <div className='mt-[30]'>
-          <h2>SALUT TEST despace</h2>
-        </div>
-
-        <div className='mt-[30]'>
-          <h2>SALUT TEST despace</h2>
-        </div>
-
-        <div className='mt-[30]'>
-          <h2>SALUT TEST despace</h2>
-        </div>
-
-        <div className='mt-[30]'>
-          <h2>SALUT TEST despace</h2>
-        </div>
-
-        <div className='mt-[30]'>
-          <h2>SALUT TEST despace</h2>
-        </div>
-
-        <div className='mt-[30]'>
-          <h2>SALUT TEST despace</h2>
-        </div>
-
-        <div className='mt-[30]'>
-          <h2>SALUT TEST despace</h2>
+      <main className="relative z-20 -mt-24 mx-4 md:mx-12 lg:mx-24 bg-white rounded-t-[50px] shadow-2xl min-h-150 p-8 md:p-12">
+        {/* Ici viendra ton contenu (recherche, filtres, produits) */}
+        <div className="text-center">
+          <h2 className="text-gray-500 font-medium">Habillez vous avec style</h2>
+          <h1 className="text-2xl md:text-3xl font-bold text-gray-900 mt-2">
+            Trouvez le style qui vous correspond parmi nos t-shirt de qualité premium
+          </h1>
         </div>
       </main>
     </div>
