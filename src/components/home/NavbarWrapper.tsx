e3621406a3f3746cb5539eba290b027dd26cb1cc
Dan <dibyjeanyves02@gmail.com>
2026-07-09 10:32:32 +0000

    feat: optimisation pour plus de fluidité

--- src/components/home/NavbarWrapper.tsx
+++ src/components/home/NavbarWrapper.tsx
@@ -1,7 +1,14 @@
-// src/components/layout/NavbarWrapper.tsx
+// src/components/home/NavbarWrapper.tsx
 'use client';
 import { useState, useEffect } from 'react';
-import { Navbar } from './Navbar';
+import dynamic from 'next/dynamic';
+
+// On importe la vraie Navbar dynamiquement en désactivant le SSR.
+// Ainsi, le composant ne s'exécutera QUE sur le client, réglant l'erreur #418 et l'alerte ESLint.
+const DynamicNavbar = dynamic(
+  () => import('./Navbar').then((mod) => mod.Navbar),
+  { ssr: false }
+);
 
 export const NavbarWrapper = () => {
   const [showNavbar, setShowNavbar] = useState(false);
@@ -12,9 +19,11 @@ export const NavbarWrapper = () => {
       setShowNavbar(window.scrollY > 120);
       setShowSearchBar(window.scrollY > 700);
     };
+
+    handleScroll();
     window.addEventListener('scroll', handleScroll);
     return () => window.removeEventListener('scroll', handleScroll);
   }, []);
 
-  return <Navbar isVisible={showNavbar} showSearchBar={showSearchBar} />;
+  return <DynamicNavbar isVisible={showNavbar} showSearchBar={showSearchBar} />;
 };
